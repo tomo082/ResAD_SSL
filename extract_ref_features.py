eeb729c703fc06d163db15a282d1f0d3065a8d20
tomo082 <131239927+tomo082@users.noreply.github.com>
2026-02-17 08:46:40 +0900

    Update extract_ref_features.py

--- extract_ref_features.py
+++ extract_ref_features.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default="mvtec")
     parser.add_argument('--few_shot_dir', type=str, default="./4shot/mvtec")
     parser.add_argument('--flow_arch', type=str, default='conditional_flow_model')
-    parser.add_argument('--bgadweight_dir', type=str, default="none")# 12/16追加
+    parser.add_argument('--bgadweight_dir', type=str, default="")# 12/16追加
     parser.add_argument('--save_dir', type=str, default="./ref_features/w50/mvtec_4shot")
     parser.add_argument('--backbone', type=str, default="wide_resnet50_2")#10/26追加
     parser.add_argument('--coupling_layers', type=int, default=10)
