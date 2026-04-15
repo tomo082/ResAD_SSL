f9657f9d8588d050f8855657be2596a9f2af79e6
tomo082 <131239927+tomo082@users.noreply.github.com>
2026-04-15 15:44:45 +0900

    Update main_vit.py

--- main_vit.py
+++ main_vit.py
@@ -125,7 +125,7 @@ def main(args):
         encoder = encoder.to(args.device)
         feat_dims = encoder.feature_info.channels()
     elif args.backbone == 'vit_base_patch14':#4/15追加
-        encoder = ViTFeatureExtractor(model_name='vit_base_patch14_224', out_indices=(3, 7, 11)).eval()
+        encoder = ViTFeatureExtractor(model_name='vit_base_patch14_reg4_dinov2.lvd142m', out_indices=(3, 7, 11)).eval()
         encoder = encoder.to(args.device)
         feat_dims = [encoder.embed_dim] * len(encoder.out_indices)
         
