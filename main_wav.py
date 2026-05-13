5426a3d49a39b6d68e8db46905433c25985015f8
tomo082 <131239927+tomo082@users.noreply.github.com>
2026-05-14 00:12:01 +0900

    Update main_wav.py

--- main_wav.py
+++ main_wav.py
@@ -9,6 +9,7 @@ import timm
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
+from train import train
 from validate_wav import validate
 from datasets.mvtec import MVTEC, MVTECANO
 from datasets.visa import VISA, VISAANO
@@ -82,45 +83,77 @@ def main(args):
     else:
         raise ValueError(f"Dataset setting must be in {SETTINGS.keys()}, but got {args.setting}.")
                 
-    if args.classes == 'capsules':  
-        train_dataset1 = CAPSULES(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize="w50", img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader1 = DataLoader(train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
-        train_dataset2 = CAPSULESANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize='w50', img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader2 = DataLoader(train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)    
-    elif CLASSES['seen'][0] in MVTEC.CLASS_NAMES:  
-        train_dataset1 = MVTEC(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize="w50", img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader1 = DataLoader(train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
-        train_dataset2 = MVTECANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize='w50', img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader2 = DataLoader(train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
-    else:  
-        train_dataset1 = VISA(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize="w50", img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader1 = DataLoader(train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
-        train_dataset2 = VISAANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, normalize="w50", img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
-        train_loader2 = DataLoader(train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
+    if args.classes == 'capsules': 
+        train_dataset1 = CAPSULES(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, 
+                               normalize="w50",
+                               img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader1 = DataLoader(
+            train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )
+        train_dataset2 = CAPSULESANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, 
+                                  normalize='w50',
+                                  img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader2 = DataLoader(
+            train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )    
+    elif CLASSES['seen'][0] in MVTEC.CLASS_NAMES: 
+        train_dataset1 = MVTEC(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, 
+                               normalize="w50",
+                               img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader1 = DataLoader(
+            train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )
+        train_dataset2 = MVTECANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, 
+                                  normalize='w50',
+                                  img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader2 = DataLoader(
+            train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )
+    else: 
+        train_dataset1 = VISA(args.train_dataset_dir, class_name=CLASSES['seen'], train=True,
+                               normalize="w50",
+                               img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader1 = DataLoader(
+            train_dataset1, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )
+        train_dataset2 = VISAANO(args.train_dataset_dir, class_name=CLASSES['seen'], train=True, 
+                                 normalize="w50",
+                                 img_size=224, crp_size=224, msk_size=224, msk_crp_size=224)
+        train_loader2 = DataLoader(
+            train_dataset2, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
+        )
         
     if args.backbone == 'wide_resnet50_2':
-        encoder = timm.create_model('wide_resnet50_2', features_only=True, out_indices=(1, 2, 3), pretrained=True).eval().to(args.device)
+        encoder = timm.create_model('wide_resnet50_2', features_only=True,
+                out_indices=(1, 2, 3), pretrained=True).eval() 
+        encoder = encoder.to(args.device)
+        feat_dims = encoder.feature_info.channels()
     elif args.backbone == 'tf_efficientnet_b6':
-        encoder = timm.create_model('tf_efficientnet_b6', features_only=True, out_indices=(1, 2, 3), pretrained=True).eval().to(args.device)
-    feat_dims = encoder.feature_info.channels()
+        encoder = timm.create_model('tf_efficientnet_b6', features_only=True,
+                out_indices=(1, 2, 3), pretrained=True).eval() 
+        encoder = encoder.to(args.device)
+        feat_dims = encoder.feature_info.channels()
         
     boundary_ops = BoundaryAverager(num_levels=args.feature_levels)
     
+    # ウェーブレットフィルタの初期化
     wav_filter = HaarWaveletFilter(low_freq_weight=args.lf_weight, high_freq_weight=args.hf_weight).to(args.device)
     wav_filter.eval()
     
+    # constraintorの初期化 (元のmain.pyに準拠)
     constraintor = MultiScaleConv(feat_dims).to(args.device)
-    optimizer0 = torch.optim.Adam(constraintor.parameters(), lr=args.lr, weight_decay=0.0005) # Weight decay
-    scheduler0 = torch.optim.lr_scheduler.MultiStepLR(optimizer0, milestones=[40, 50], gamma=0.1) # スケジューラ前倒し
+    optimizer0 = torch.optim.Adam(constraintor.parameters(), lr=args.lr, weight_decay=0.0005)
+    scheduler0 = torch.optim.lr_scheduler.MultiStepLR(optimizer0, milestones=[70, 90], gamma=0.1)
     
-    estimators = [load_flow_model(args, feat_dim).to(args.device) for feat_dim in feat_dims]
+    # NFの初期化
+    estimators = [load_flow_model(args, feat_dim) for feat_dim in feat_dims]
+    estimators = [decoder.to(args.device) for decoder in estimators]
     params = list(estimators[0].parameters())
     for l in range(1, args.feature_levels):
         params += list(estimators[l].parameters())
-    optimizer1 = torch.optim.Adam(params, lr=args.lr, weight_decay=0.005)
-    scheduler1 = torch.optim.lr_scheduler.MultiStepLR(optimizer1, milestones=[30, 50], gamma=0.1)
+    optimizer1 = torch.optim.Adam(params, lr=args.lr, weight_decay=0.0005)
+    scheduler1 = torch.optim.lr_scheduler.MultiStepLR(optimizer1, milestones=[70, 90], gamma=0.1)
     
-    from train import train
     best_img_auc = 0
     N_batch = 8192
     
@@ -129,56 +162,53 @@ def main(args):
         for estimator in estimators:
             estimator.train()
             
-        train_loader = train_loader1 if epoch < FIRST_STAGE_EPOCH else train_loader2
+        if epoch < FIRST_STAGE_EPOCH:
+            train_loader = train_loader1
+        else:
+            train_loader = train_loader2
+            
         train_loss_total, total_num = 0, 0
-        
-        progress_bar = tqdm(total=len(train_loader), desc=f"Epoch[{epoch}/{args.epochs}]")
+        progress_bar = tqdm(total=len(train_loader))
+        progress_bar.set_description(f"Epoch[{epoch}/{args.epochs}]")
         
         for step, batch in enumerate(train_loader):
             progress_bar.update(1)
             images, _, masks, class_names = batch
-            images, masks = images.to(args.device), masks.to(args.device)
+            
+            images = images.to(args.device)
+            masks = masks.to(args.device)
             
             with torch.no_grad():
-                # 1. 画像から特徴抽出
                 features = encoder(images)
-                
-                # 2. テスト画像(訓練バッチ)の特徴量をウェーブレット変換 (Pre-filter)
+                # --- ウェーブレット変換 (Pre-filter) ---
                 features = [wav_filter(f) for f in features]
-                
-                # 3. 訓練用の参照特徴量を抽出し、それらもウェーブレット変換
-                ref_features = get_mc_reference_features_wav(encoder, args.train_dataset_dir, class_names, images.device, args.train_ref_shot,wav_filter=wav_filter)
-
-                
-                # 4. 「エッジ強調された特徴量同士」でマッチング
-                mfeatures = get_mc_matched_ref_features(features, class_names, ref_features)
-                
-                # 5. 残差の計算
-                rfeatures = get_residual_features(features, mfeatures, pos_flag=True)
+            
+            # --- utilsの関数内で自動変換させる ---
+            ref_features = get_mc_reference_features_wav(encoder, args.train_dataset_dir, class_names, images.device, args.train_ref_shot, wav_filter=wav_filter)
+            mfeatures = get_mc_matched_ref_features(features, class_names, ref_features)
+            rfeatures = get_residual_features(features, mfeatures, pos_flag=True)
             
             lvl_masks = []
             for l in range(args.feature_levels):
                 _, _, h, w = rfeatures[l].size()
-                lvl_masks.append(F.interpolate(masks, size=(h, w), mode='nearest').squeeze(1))
+                m = F.interpolate(masks, size=(h, w), mode='nearest').squeeze(1)
+                lvl_masks.append(m)
             rfeatures_t = [rfeature.detach().clone() for rfeature in rfeatures]
             
-            # 6. Constraintorによる空間補正
+            # constraintor 適用
             rfeatures = constraintor(*rfeatures)
-            
-            # (任意) 特徴量への微小ノイズ付加による過学習防止
-            #noise_std = 0.01
-            #rfeatures_noisy = [rf + torch.randn_like(rf) * noise_std for rf in rfeatures]
-            
             loss = 0
             for l in range(args.feature_levels):  
                 e = rfeatures[l]  
                 t = rfeatures_t[l]
                 bs, dim, h, w = e.size()
-                e, t = e.permute(0, 2, 3, 1).reshape(-1, dim), t.permute(0, 2, 3, 1).reshape(-1, dim)
-                m = lvl_masks[l].reshape(-1)
+                e = e.permute(0, 2, 3, 1).reshape(-1, dim)
+                t = t.permute(0, 2, 3, 1).reshape(-1, dim)
+                m = lvl_masks[l]
+                m = m.reshape(-1)
+                
                 loss_i, _, _ = calculate_log_barrier_bi_occ_loss(e, m, t)
                 loss += loss_i
-                
             optimizer0.zero_grad()
             loss.backward()
             optimizer0.step()
@@ -193,14 +223,13 @@ def main(args):
         
         scheduler0.step()
         scheduler1.step()
+               
         progress_bar.close()
         print(f"Epoch[{epoch}/{args.epochs}]: train_loss: {train_loss_total / total_num}")
         
         # --- 評価フェーズ ---
         if (epoch + 1) % args.eval_freq == 0:
             s1_res, s2_res, s_res = [], [], []
-            
-            # 既にextract_ref_features_wav.pyでウェーブレット変換済みのカンペをロード
             test_ref_features = load_mc_reference_features(args.test_ref_feature_dir, CLASSES['unseen'], args.device, args.num_ref_shot)
             
             for class_name in CLASSES['unseen']:
@@ -225,17 +254,30 @@ def main(args):
                 
                 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=8, drop_last=False)
                 
-                # validate_wavに wav_filter を渡す
+                # validate関数に wav_filter を渡す
                 metrics = validate(args, encoder, constraintor, wav_filter, estimators, test_loader, test_ref_features[class_name], args.device, class_name)
                 
                 img_auc, img_ap, img_f1_score, pix_auc, pix_ap, pix_f1_score, pix_aupro = metrics['scores']
-                print("Epoch: {}, Class Name: {}, Image AUC: {:.3f} | Pixel AUC: {:.3f} | AUPRO: {:.3f}".format(
-                    epoch, class_name, img_auc, pix_auc, pix_aupro))
+                
+                # 元の main.py と同じ詳細な print 文
+                print("Epoch: {}, Class Name: {}, Image AUC | AP | F1_Score: {} | {} | {}, Pixel AUC | AP | F1_Score | AUPRO: {} | {} | {} | {}".format(
+                    epoch, class_name, img_auc, img_ap, img_f1_score, pix_auc, pix_ap, pix_f1_score, pix_aupro))
                 s1_res.append(metrics['scores1'])
                 s2_res.append(metrics['scores2'])
                 s_res.append(metrics['scores'])
             
-            img_auc, img_ap, img_f1_score, pix_auc, pix_ap, pix_f1_score, pix_aupro = np.mean(np.array(s_res), axis=0)
+            s1_res = np.array(s1_res)
+            s2_res = np.array(s2_res)
+            s_res = np.array(s_res)
+            img_auc1, img_ap1, img_f1_score1, pix_auc1, pix_ap1, pix_f1_score1, pix_aupro1 = np.mean(s1_res, axis=0)
+            img_auc2, img_ap2, img_f1_score2, pix_auc2, pix_ap2, pix_f1_score2, pix_aupro2 = np.mean(s2_res, axis=0)
+            img_auc, img_ap, img_f1_score, pix_auc, pix_ap, pix_f1_score, pix_aupro = np.mean(s_res, axis=0)
+            
+            # 元の main.py と同じ平均値の print 文
+            print('(Logps) Average Image AUC | AP | F1_Score: {:.3f} | {:.3f} | {:.3f}, Average Pixel AUC | AP | F1_Score | AUPRO: {:.3f} | {:.3f} | {:.3f} | {:.3f}'.format(
+                img_auc1, img_ap1, img_f1_score1, pix_auc1, pix_ap1, pix_f1_score1, pix_aupro1))
+            print('(BScores) Average Image AUC | AP | F1_Score: {:.3f} | {:.3f} | {:.3f}, Average Pixel AUC | AP | F1_Score | AUPRO: {:.3f} | {:.3f} | {:.3f} | {:.3f}'.format(
+                img_auc2, img_ap2, img_f1_score2, pix_auc2, pix_ap2, pix_f1_score2, pix_aupro2))
             print('(Merged) Average Image AUC | AP | F1_Score: {:.3f} | {:.3f} | {:.3f}, Average Pixel AUC | AP | F1_Score | AUPRO: {:.3f} | {:.3f} | {:.3f} | {:.3f}'.format(
                 img_auc, img_ap, img_f1_score, pix_auc, pix_ap, pix_f1_score, pix_aupro))
             
@@ -245,6 +287,7 @@ def main(args):
                 state_dict = {'constraintor': constraintor.state_dict(),
                               'estimators': [estimator.state_dict() for estimator in estimators]}
                 torch.save(state_dict, os.path.join(args.checkpoint_path, f'{args.setting}_epoch_{epoch}_checkpoints.pth'))
+
 
 def load_mc_reference_features(root_dir: str, class_names, device: torch.device, num_shot=4):
     refs = {}
@@ -258,39 +301,44 @@ def load_mc_reference_features(root_dir: str, class_names, device: torch.device,
         refs[class_name] = (layer1_refs[:K1, :], layer2_refs[:K2, :], layer3_refs[:K3, :])
     return refs
                     
+                    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--setting', type=str, default="visa_to_mvtec")
     parser.add_argument('--classes', type=str, default="none")
     parser.add_argument('--train_dataset_dir', type=str, default="")
     parser.add_argument('--test_dataset_dir', type=str, default="")
-    parser.add_argument('--test_ref_feature_dir', type=str, default="./ref_features/w50/mvtec_4shot_wav") # デフォルトを _wav に変更
+    parser.add_argument('--test_ref_feature_dir', type=str, default="./ref_features/w50/mvtec_4shot_wav")
     parser.add_argument('--bgadweight_dir', type=str, default="none")
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--lr', type=float, default=1e-5)
-    parser.add_argument('--epochs', type=int, default=60) # 過学習防止のためエポック数を削減
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--device', type=str, default="cuda:0")
     parser.add_argument('--checkpoint_path', type=str, default="./checkpoints/")
     parser.add_argument('--eval_freq', type=int, default=1)
     parser.add_argument('--backbone', type=str, default="wide_resnet50_2")
+    parser.add_argument('--rank', type=int, default="0")    
     
+    # flow parameters
     parser.add_argument('--flow_arch', type=str, default='conditional_flow_model')
     parser.add_argument('--feature_levels', default=3, type=int)
-    parser.add_argument('--pos_embed_dim', type=int, default=256)
-    parser.add_argument("--train_ref_shot", type=int, default=4)
-    parser.add_argument("--num_ref_shot", type=int, default=4)
-    parser.add_argument('--pos_beta', type=float, default=0.05)            
     parser.add_argument('--coupling_layers', type=int, default=10)
-    parser.add_argument('--clamp_alpha', type=float, default=1.9)    
+    parser.add_argument('--clamp_alpha', type=float, default=1.9)
+    parser.add_argument('--pos_embed_dim', type=int, default=256)
+    parser.add_argument('--pos_beta', type=float, default=0.05)
     parser.add_argument('--margin_tau', type=float, default=0.1)
     parser.add_argument('--bgspp_lambda', type=float, default=1)
-    parser.add_argument('--fdm_alpha', type=float, default=0.4)
+    
+    parser.add_argument('--fdm_alpha', type=float, default=0.4) 
     parser.add_argument('--num_embeddings', type=int, default=1536)
-            
-    # ウェーブレット用パラメータ
-    parser.add_argument("--lf_weight", type=float, default=0.1, help="Weight for low frequency (LL)")
-    parser.add_argument("--hf_weight", type=float, default=1.2, help="Weight for high frequency (LH, HL, HH)")
+    parser.add_argument("--train_ref_shot", type=int, default=4)
+    parser.add_argument("--num_ref_shot", type=int, default=4)
+    
+    # --- 追加: ウェーブレット用パラメータ ---
+    parser.add_argument("--lf_weight", type=float, default=0.1)
+    parser.add_argument("--hf_weight", type=float, default=1.2)
     
     args = parser.parse_args()
     init_seeds(42)
+    
     main(args)
